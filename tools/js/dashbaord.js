05c8991a58f86fe82757bacf3b5500079712bbc5
bharathswamygowda <bharathsgowda111@gmail.com>
2022-02-26 21:41:29 +0530

    Ui changes

--- tools/js/dashbaord.js
+++ tools/js/dashbaord.js
@@ -2,14 +2,14 @@ var category = $(".category").select2({
     dropdownParent: $('#createCampaign'),
     placeholder: "Make a selection",
     allowClear: true,
-    tags: true,
-    closeOnSelect: true,
+    tags: false,
+    closeOnSelect: false,
 });
 
 var followers = $(".followers").select2({
     dropdownParent: $('#createCampaign'),
     placeholder: "Make a selection",
     allowClear: true,
-    tags: true,
-    closeOnSelect: true,
+    tags: false,
+    closeOnSelect: false,
 });
